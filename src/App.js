195a81be3e36f2ece323dbe3994443e09cdefdf7
vishal123test <99317054+vishal123test@users.noreply.github.com>
2022-02-26 15:26:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -59,7 +59,7 @@ export const ResponsiveWrapper = styled.div`
   flex-direction: column;
   justify-content: stretched;
   align-items: stretched;
-  width: 100%;
+  width: 50%;
   @media (min-width: 767px) {
     flex-direction: row;
   }
