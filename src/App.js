eb4c1566c21e038e411b537e705a8461669a9318
vishal123test <99317054+vishal123test@users.noreply.github.com>
2022-02-26 09:19:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -418,9 +418,7 @@ function App() {
               color: "var(--primary-text)",
             }}
           >
-            Please make sure you are connected to the right network (
-            {CONFIG.NETWORK.NAME} Mainnet) and the correct address. Please note:
-            Once you make the purchase, you cannot undo this action.
+            2022 - NOT AFFLIATED WITH NBA JAM OR ANY OTHER NFT PROJECT
           </s.TextDescription>
           <s.SpacerSmall />
           <s.TextDescription
@@ -429,9 +427,6 @@ function App() {
               color: "var(--primary-text)",
             }}
           >
-            We have set the gas limit to {CONFIG.GAS_LIMIT} for the contract to
-            successfully mint your NFT. We recommend that you don't lower the
-            gas limit.
           </s.TextDescription>
         </s.Container>
       </s.Container>
