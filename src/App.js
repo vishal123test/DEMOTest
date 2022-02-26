ed2231168a8a4cbffeceb6a0a75d10ecb729e120
vishal123test <99317054+vishal123test@users.noreply.github.com>
2022-02-26 12:07:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -239,13 +239,12 @@ function App() {
             jc={"center"}
             ai={"center"}
             style={{
-              backgroundColor: "var(--accent)"
+              backgroundColor: "var(--accent)",
               padding: 24,
               borderRadius: 24,
               border: "4px dashed var(--secondary)",
               boxShadow: "0px 5px 11px 2px rgba(0,0,0,0.7)",
             }}
-            
           >
             <s.TextTitle
               style={{
