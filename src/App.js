2d4ed7972e55f9f422ab8a441fa246f2df0a598a
vishal123test <99317054+vishal123test@users.noreply.github.com>
2022-03-17 10:17:04 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -106,6 +106,7 @@ function App() {
   const blockchain = useSelector((state) => state.blockchain);
   const data = useSelector((state) => state.data);
   const [claimingNft, setClaimingNft] = useState(false);
+  const [feedback, setFeedback] = useState();
   const [mintAmount, setMintAmount] = useState(1);
   const [CONFIG, SET_CONFIG] = useState({
     CONTRACT_ADDRESS: "",
