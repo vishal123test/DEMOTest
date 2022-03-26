c63306409cbbc66fb115d3d927ff871d0d141e91
vishal123test <99317054+vishal123test@users.noreply.github.com>
2022-03-26 11:51:00 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -281,7 +281,6 @@ function App() {
               boxShadow: "inset 0px 0px 0px 10px #226a7d",
               height: "535px",
               width: "235px",
-              margin-left: "-115px"
             }}
           >
             <s.TextTitle
