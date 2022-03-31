794865dab64390048fd054838b3afec8bf2c04fb
vishal123test <99317054+vishal123test@users.noreply.github.com>
2022-03-31 09:32:37 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -416,22 +416,6 @@ function App() {
                         color: "var(--accent-text)",
                       }}
                     >
-                     
-                    </s.TextDescription>
-
-                    <s.TextDescription
-                    style={{
-                      textAlign: "center",
-                      color: "#dad748",
-                      fontFamily: "var(--accent-font-family)",
-                      opacity: "1",
-                      margin: "0 76px 0 0"     
-                    }}
-                  >
-                   CONNECTED
-                  </s.TextDescription>
-
-                  <StyledLogo alt={"redbar"} src={"/config/images/redbar.png"} />
 
                     {blockchain.errorMsg !== "" ? (
                       <>
