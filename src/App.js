8ae8381e9eec40f02700d12d1c02c5e0d90ac2bd
vishal123test <99317054+vishal123test@users.noreply.github.com>
2022-03-31 11:26:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -66,6 +66,19 @@ export const ResponsiveWrapper = styled.div`
 `;
 
 export const StyledLogo = styled.img`
+  @media (min-width: 767px) {
+  }
+  transition: width 0.5s;
+  transition: height 0.5s;
+  width: 143px;
+    height: 10px;
+    margin-top: 0px;
+    border: 1px solid rgb(99, 133, 150);
+    box-shadow: 3px -10px inset #000;
+    margin: 0 58px 0 0;
+`;
+
+export const StyledLogo = styled.img`
   width: 200px;
   @media (min-width: 767px) {
     width: 300px;
