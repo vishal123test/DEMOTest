5b78d053d982251fc08f524a63916498ac707095
vishal123test <99317054+vishal123test@users.noreply.github.com>
2022-03-31 12:28:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -103,8 +103,7 @@ export const StyledBar = styled.img`
   }
   transition: width 0.5s;
   transition: height 0.5s;
-  width: 1922px;
-  height: 60px;
+  width: 100%;
   margin-top: 0px;
   border-top: 2px solid #2a11d4;
   box-shadow: rgb(0 0 0) 3px -10px inset;
