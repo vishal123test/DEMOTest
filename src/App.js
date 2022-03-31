a06c7c6c81d82bd1ac84e47e533bf1d6fa449190
vishal123test <99317054+vishal123test@users.noreply.github.com>
2022-03-31 16:02:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -233,7 +233,7 @@ function App() {
           </s.Container>
           <s.SpacerLarge />
           <s.Container
-            flex={0}
+            flex={2}
             jc={"center"}
             ai={"center"}
             style={{
