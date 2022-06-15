d7297a7f0f66df5ce7ab6a33b79da047512a6273
vishal123test <99317054+vishal123test@users.noreply.github.com>
2022-06-15 12:00:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -339,11 +339,40 @@ function App() {
                       MINT
                     </StyledButtons>
 
-                      <StyledInput
-                          
-                        >
-                          MINT
-                    </StyledInput>
+                      const TextInput = props => {
+                        const { setFields, addField } = useContext(FormCtx);
+                        const {
+                          id,
+                          value,
+                          classes,
+                          onChange,
+                          validate,
+                          placeholder,
+                          label = ""
+                        } = props;
+                        const { contClass, fieldClass, errorClass } = classes;
+  
+                        useEffect(() => {
+                        addField({
+                          field: props,
+                          value
+                        });
+                      }, []);
+
+                      return (
+                        <div class={contClass}>
+                          {label}
+                          <input
+                            id={id}
+                            type="text"
+                            value={value}
+                            class={fieldClass}
+                            onChange={onChange}
+                            validatge={validate}
+                            placeholder={placeholder}
+                          />
+                          <p class={errorClass}>// place for errors</p>
+                        </div>
 
                     <s.TextDescription
                       style={{
