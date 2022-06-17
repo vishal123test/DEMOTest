42010b970fb19b667209c276390319ee5d4f8afe
vishal123test <99317054+vishal123test@users.noreply.github.com>
2022-06-17 10:13:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { connect } from "./redux/blockchain/blockchainActions";
 import { fetchData } from "./redux/data/dataActions";
-import { TextField } from "@material-ui/core";
 import * as s from "./styles/globalStyles";
 import styled from "styled-components";
 
